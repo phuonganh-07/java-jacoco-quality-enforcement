11399279ce35e1cfa73ab7c089a6712c3d9f66f4
Admin <25023432@vnu.edu.vn>
2026-04-27 23:02:49 +0700

    change

--- src/test/java/com/auction/service/AuctionServiceTest.java
+++ src/test/java/com/auction/service/AuctionServiceTest.java
@@ -13,19 +13,16 @@ public class AuctionServiceTest {
 
   @Test
   public void testHighValueStandardFee() {
-    // Bao phủ nhánh bidAmount >= 1000
     assertEquals(50, service.calculateFee(1000, false));
   }
 
   @Test
   public void testSmallValueStandardFee() {
-    // Bao phủ nhánh bidAmount > 0
     assertEquals(10, service.calculateFee(100, false));
   }
 
   @Test
   public void testInvalidBid() {
-    // Bao phủ nhánh cuối cùng (return 0)
     assertEquals(0, service.calculateFee(-1, false));
   }
 }
